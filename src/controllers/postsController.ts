053718575b552a31aea69a482b8f96c7dbb06315
Fer <sombondrolo@gmail.com>
2024-03-13 01:21:10 -0300

    src/controllers/postsController.ts: using NF API /posts endpoint; vercel.json: file created to prevent 404 not_found

--- src/controllers/postsController.ts
+++ src/controllers/postsController.ts
@@ -7,7 +7,8 @@ export interface Post {
 // GET /posts => /posts/+page.svelte
 export async function getPosts(): Promise<Post[]> {
   try {
-    const response = await fetch('http://127.0.0.1:3000/posts')
+    // const response = await fetch('http://127.0.0.1:3000/posts')
+    const response = await fetch('https://back--go-fiber-api--f98q4y48vb47.code.run/posts')
     if (!response.ok) {
       throw new Error('Network response was not ok')
     }
